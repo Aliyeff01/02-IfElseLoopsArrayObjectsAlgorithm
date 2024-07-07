0f1580e7106ca87329b2b900fc4767d48dcf2935
Aliyeff01 <arazpal@code.edu.az>
2024-07-07 14:12:45 +0400

    Add files via upload

---
+++ Task5/script.js
@@ -0,0 +1,13 @@
+let arr = [3, 8, 2, 5, 4, 10, 7, 6];
+
+let min = arr[0];
+let minIndex = 0;
+
+for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < min) {
+        min = arr[i];
+        minIndex = i;
+    }
+}
+
+console.log( minIndex);
